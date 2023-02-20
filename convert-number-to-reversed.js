3e62850862741715cc67b0db4b21e38019318218
JACedwards <98490993+JACedwards@users.noreply.github.com>
2023-02-20 10:31:47 -0500

    reverse()_practice

---
+++ convert-number-to-reversed.js
@@ -0,0 +1,27 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+function digitize(n) {
+    let str = '';
+    str = n.toString();
+    let arr = str.split("");
+    console.log(arr);
+    arr.reverse();
+    let output = []
+    for (let i=0; i < arr.length; i++){
+        output.push(parseInt(arr[i]));
+    
+    }
+    return output;
+}
+
+console.log(digitize(35231))
+
+
+// let text = "35231";
+// const myArray = text.split("");
+// console.log(myArray)
--- test.js
+++ test.js
@@ -1,18 +1,36 @@
-let strng = "10 65 74 100 99 68 86 180 90"
+// let strng = "10 65 74 100 99 68 86 180 90"
 
-let nums = {1:'one', 10:'ten', 11: 'eleven', 12: 'tw', 13: 'th', 14:'fo', 15:'fi', 16: 'si', 17:'se', 18:'ei', 19:'ni', 2: 'tw', 3: 'th', 4 : 'fo', 5 : 'fi', 6 : 'si', 7:'se', 8:'ei', 9:'ni'}
+// let nums = {1:'one', 10:'ten', 11: 'eleven', 12: 'tw', 13: 'th', 14:'fo', 15:'fi', 16: 'si', 17:'se', 18:'ei', 19:'ni', 2: 'tw', 3: 'th', 4 : 'fo', 5 : 'fi', 6 : 'si', 7:'se', 8:'ei', 9:'ni'}
 
-let alphas = []
-let alpha = ''
+// let alphas = []
+// let alpha = ''
 
-for (let l = 0; l < strng.length; l++){
-    for (let m = 0; m <strng[l].length; m++) {
-        if (strng[1] == '10') {
-            alpha = nums[10];
-            console.log(alpha)
-        }
+// for (let l = 0; l < strng.length; l++){
+//     for (let m = 0; m <strng[l].length; m++) {
+//         if (strng[1] == '10') {
+//             alpha = nums[10];
+//             console.log(alpha)
+//         }
 
-    }
-}
-alpha = nums[10];
-console.log(alpha)
+//     }
+// }
+// alpha = nums[10];
+// console.log(alpha)
+
+// let arr = [1, 2, 3, 4];
+// arr.every((elem, index, arr) => {
+//   arr[index]--;
+//   console.log(`[${arr}][${index}] -> ${elem}`);
+//   console.log(elem < 2);
+// });
+
+arr = ['1', '2', '3'];
+let output = []
+
+arr.every((elem, index, arr) => {
+  output.push(parseInt(elem));
+  console.log(output);
+  return elem < arr.length + 1;
+});
+
+
